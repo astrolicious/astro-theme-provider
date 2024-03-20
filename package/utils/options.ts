8c9c09b90bc94809ff83438f1adefa54f936cb3f
Bryce Russell <brycetrussell@gmail.com>
2024-03-20 09:46:33 -0500

    Update `mergeOptions` to merge directly on to the target

--- package/utils/options.ts
+++ package/utils/options.ts
@@ -1,31 +1,29 @@
 export function mergeOptions(target: Record<any, any>, source: Record<any, any>) {
-	const merge = { ...target };
-
 	for (const key in source) {
 		const value = source[key];
 
 		if (typeof value === "object" && value !== null) {
-			if (Array.isArray(value) && Array.isArray(merge[key])) {
+			if (Array.isArray(value) && Array.isArray(target[key])) {
 				// Combine array values
-				merge[key].push(...value);
+				target[key].push(...value);
 				continue;
 			}
 
 			if (
-				typeof merge[key] === "object" &&
-				merge[key] !== null &&
+				typeof target[key] === "object" &&
+				target[key] !== null &&
 				// Skip zod schemas
 				!("_def" in value)
 			) {
 				// Combine object values
-				merge[key] = mergeOptions(merge[key], value);
+				target[key] = mergeOptions(target[key], value);
 				continue;
 			}
 		}
 
 		// Overwrite all other values
-		merge[key] = value;
+		target[key] = value;
 	}
 
-	return merge;
+	return target;
 }
