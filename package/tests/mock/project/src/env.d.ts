9abaf11a1182178620a7a5e027dea668a11ea194
Bryce Russell <brycetrussell@gmail.com>
2024-04-01 18:18:42 -0500

    Update ignore list for changeset config

--- package/tests/mock/project/src/env.d.ts
+++ package/tests/mock/project/src/env.d.ts
@@ -1,4 +1,4 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
-/// <reference types="../.astro/mock-theme.d.ts" />
+/// <reference types="../.astro/theme-mock.d.ts" />
 /// <reference types="../.astro/theme-playground.d.ts" />
