9bb88491afb3aebf8f9e8b47bceabae1ede0bd7b
Bryce Russell <brycetrussell@gmail.com>
2024-05-12 09:01:15 -0500

    Remove test code

--- tests/themes/theme-playground/index.ts
+++ tests/themes/theme-playground/index.ts
@@ -2,14 +2,6 @@ import sitemap from "@astrojs/sitemap";
 import defineTheme from "astro-theme-provider";
 import { z } from "astro/zod";
 
-const schema = z.object({
-	title: z.string(),
-	description: z.string().optional(),
-	sitemap: z.boolean().optional().default(true),
-});
-
-type TTT = z.input<typeof schema>;
-
 export default defineTheme({
 	name: "theme-playground",
 	schema: z.object({
