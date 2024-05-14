12e89bde6ea271198f563b4dfa40cba2861f59c6
Bryce Russell <brycetrussell@gmail.com>
2024-05-14 09:46:21 -0500

    Remove test file

--- tests/themes/theme-playground/astro.config.mjs
+++
@@ -1,7 +0,0 @@
-import sitemap from "@astrojs/sitemap";
-import { defineConfig } from "astro/config";
-
-// https://astro.build/config
-export default defineConfig({
-	integrations: [sitemap()],
-});
